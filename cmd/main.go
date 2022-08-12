f6db6ea707925e404c288a32081ab72f603a10de
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-08-12 22:11:03 +0300

    Update main app is running with uber-fx

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"fmt"
-
-	log "github.com/sirupsen/logrus"
+	"go.uber.org/fx"
 
 	"github.com/kaankoken/binance-quick-go-api-gateway/config"
 	"github.com/kaankoken/binance-quick-go-api-gateway/pkg/helper"
 )
 
-func init() {
-	log.SetFormatter(&log.TextFormatter{})
-}
-
 func main() {
-	c, err := config.LoadConfig()
-	helper.CheckError(err)
+	app := fx.New(helper.Module, config.Module)
 
-	fmt.Println(c.Port)
+	app.Run()
 }
