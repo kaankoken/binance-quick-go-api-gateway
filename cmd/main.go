6667cc648a052f824769a5776504a14d95dcca7d
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-08-16 17:53:28 +0300

    Update telegram api gateway & gin server allocation is completed

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+
 	"go.uber.org/fx"
 
 	"github.com/kaankoken/binance-quick-go-api-gateway/config"
@@ -12,8 +14,26 @@ func main() {
 	app := fx.New(
 		helper.Module,
 		config.Module,
-		telegram.Module,
+		telegram.EngineModule,
+		telegram.RouteModule,
+		fx.Invoke(registerHooks),
 	)
 
 	app.Run()
+}
+
+func registerHooks(lifecycle fx.Lifecycle, h *telegram.Handler, config *config.Config, logger *helper.Handler) {
+	lifecycle.Append(
+		fx.Hook{
+			OnStart: func(context.Context) error {
+				logger.Info("Starting application in " + config.Port)
+				go h.Gin.Run(config.Port)
+				return nil
+			},
+			OnStop: func(context.Context) error {
+				logger.Info("Stopping application")
+				return nil
+			},
+		},
+	)
 }
