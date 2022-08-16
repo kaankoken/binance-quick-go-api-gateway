86a10a2748f244e3cc7a1ccee0bc8b6a0969838a
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-08-16 17:53:28 +0300

    Update telegram api gateway & gin server allocation is completed

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 
+	"go.uber.org/fx"
+
 	"github.com/kaankoken/binance-quick-go-api-gateway/config"
 	"github.com/kaankoken/binance-quick-go-api-gateway/pkg/helper"
 	"github.com/kaankoken/binance-quick-go-api-gateway/pkg/telegram"
