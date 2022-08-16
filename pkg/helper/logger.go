b2e9e445b882042637343c99769dc7e962b93994
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-08-16 17:49:32 +0300

    Update error & info messages seperated on logger

--- pkg/helper/logger.go
+++ pkg/helper/logger.go
@@ -5,23 +5,32 @@ import (
 	"go.uber.org/fx"
 )
 
-var Module = fx.Options(fx.Invoke(SetLoggerFormat), fx.Provide(CheckError))
+var Module = fx.Options(
+	fx.Invoke(SetLoggerFormat),
+	fx.Provide(Logger),
+)
 
 func SetLoggerFormat() {
 	log.SetFormatter(&log.TextFormatter{})
 }
 
 type Handler struct {
-	Logger func(error, *func())
+	Error func(error, *func())
+	Info  func(msg string)
 }
 
-func CheckError() *Handler {
-	return &Handler{Logger: func(err error, f *func()) {
-		if err != nil {
-			log.Fatalln("Binance-Quick-Go-Api-Gateway -> ", err.Error())
-			if f != nil {
-				(*f)()
+func Logger() *Handler {
+	return &Handler{
+		Error: func(err error, f *func()) {
+			if err != nil {
+				log.Fatalln("Binance-Quick-Go-Api-Gateway -> ", err.Error())
+				if f != nil {
+					(*f)()
+				}
 			}
-		}
-	}}
+		},
+		Info: func(msg string) {
+			log.Infoln("Binance-Quick-Go-Api-Gateway -> ", msg)
+		},
+	}
 }
