b5f368bd6342e9f41725ead5521556716fc48cd8
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-08-16 17:51:15 +0300

    Update gin context is removed due to not needed

--- pkg/telegram/handler.go
+++ pkg/telegram/handler.go
@@ -8,11 +8,10 @@ import (
 var EngineModule = fx.Options(fx.Provide(NewHandler))
 
 type Handler struct {
-	Gin     *gin.Engine
-	Context *gin.Context
+	Gin *gin.Engine
 }
 
 func NewHandler() *Handler {
-	handler := Handler{Gin: gin.Default(), Context: &gin.Context{}}
+	handler := Handler{Gin: gin.Default()}
 	return &handler
 }
