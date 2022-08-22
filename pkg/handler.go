236868a73f7c7e61559e99e44996893ee5d32c9f
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-08-22 17:30:55 +0300

    Add/Update  gin engine handler test(unit & di) added & module mae it more testable

--- pkg/handler.go
+++ pkg/handler.go
@@ -1,20 +1,25 @@
 package pkg
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kaankoken/binance-quick-go-api-gateway/config"
 	"go.uber.org/fx"
 )
 
-var EngineModule = fx.Options(fx.Provide(newHandler))
+var EngineModule = fx.Options(fx.Provide(NewHandler))
 
 type Handler struct {
 	Gin *gin.Engine
 }
 
-func newHandler(config *config.Config) *Handler {
+func NewHandler(config *config.Config) (h *Handler, err error) {
+	if gin.DebugMode != config.Mode && gin.TestMode != config.Mode && gin.ReleaseMode != config.Mode {
+		return nil, fmt.Errorf("incorrect mode type")
+	}
+
 	gin.SetMode(config.Mode)
 
-	handler := Handler{Gin: gin.Default()}
-	return &handler
+	return &Handler{Gin: gin.Default()}, nil
 }
